10a11ea3961f55071a6f4a6d2bce2d24f67fd2a4
Philip Hayes <philiphayes9@gmail.com>
2013-10-20 12:28:26 -0700

    Fill out Operator Interface header.

---
+++ src/OI.h
@@ -0,0 +1,30 @@
+/*
+ * Wires commands to operator inputs.
+ */
+
+#ifndef _OI_H
+#define _OI_H
+
+#include "Joystick.h"
+#include "Buttons/JoystickButton.h"
+
+class OI {
+
+ public:
+  OI();
+  ~OI();
+
+ private:
+  // Joysticks
+  Joystick drive_stick;
+
+  // Buttons on Joysticks
+  JoystickButton solenoid_up_button;
+  JoystickButton solenoid_down_button;
+
+  JoystickButton turn_left_button;
+  JoystickButton turn_right_button;
+
+};
+
+#endif
