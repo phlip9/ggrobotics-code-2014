76bdc0ec996ed22ada56ad9264d5c03aee51a736
Philip Hayes <philiphayes9@gmail.com>
2014-01-11 10:02:55 -0800

    Use pointers from hardware map.

--- src/Subsystems/Drive.cpp
+++ src/Subsystems/Drive.cpp
@@ -2,18 +2,16 @@
 
 #include "Drive.h"
 
-#include "Joystick.h"
-
 #include "../Commands/BackgroundDrive.h"
 
 Drive::Drive(const HardwareMap& hardware_map)
     : Subsystem("Drive"),
-      robot_drive(&hardware_map.front_left_motor,
-                  &hardware_map.rear_left_motor,
-                  &hardware_map.front_right_motor,
-                  &hardware_map.rear_right_motor) {
+      robot_drive(hardware_map.front_left_motor,
+                  hardware_map.rear_left_motor,
+                  hardware_map.front_right_motor,
+                  hardware_map.rear_right_motor) {
 
-  robot_drive.setExpiration(0.1);
+  robot_drive.SetExpiration(0.1);
 }
 
 Drive::~Drive() { }
@@ -22,7 +20,7 @@ void Drive::InitDefaultCommand() {
   SetDefaultCommand(new BackgroundDrive());
 }
 
-void Drive::mecanum_drive(const Joystick& drive_stick) {
+void Drive::mecanum_drive(Joystick& drive_stick) {
   float magnitude, direction, rotation;
   magnitude = drive_stick.GetMagnitude();
   direction = drive_stick.GetDirectionDegrees();
