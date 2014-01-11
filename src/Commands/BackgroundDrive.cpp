df411820b76f1939b9e722717b8007f63eb8328c
Philip Hayes <philiphayes9@gmail.com>
2014-01-11 10:01:34 -0800

    Pointerzzz

--- src/Commands/BackgroundDrive.cpp
+++ src/Commands/BackgroundDrive.cpp
@@ -8,7 +8,7 @@
 BackgroundDrive::BackgroundDrive()
   : Command("BackgroundDrive") {
 
-  Requires(&Robot::instance().drive);
+  Requires(&(Robot::instance().drive));
 }
 
 void BackgroundDrive::Initialize() {
@@ -16,7 +16,7 @@ void BackgroundDrive::Initialize() {
 }
 
 void BackgroundDrive::Execute() {
-  Robot* robot = &Robot::instance();
+  Robot *robot = &Robot::instance();
   robot->drive.mecanum_drive(robot->oi.drive_stick);
 }
 
