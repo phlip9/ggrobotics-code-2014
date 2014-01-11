9a6777d1a2eabcb56701ec457db08a5ead3a1a8c
Philip Hayes <philiphayes9@gmail.com>
2014-01-11 10:03:54 -0800

    Add DriverStationLCD code and debug messages.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,43 +5,71 @@
 
 #include "Robot.h"
 
+#include <stdio.h>
+
 #include "Commands/Scheduler.h"
 #include "LiveWindow/LiveWindow.h"
+#include "DriverStationLCD.h"
+#include "Commands/PrintCommand.h"
 
 Robot::Robot()
   : hardware_map(),
     oi(),
     drive(hardware_map),
-    autonomous_command(),
-    teleop_command() {
+    ds_lcd(DriverStationLCD::GetInstance()),
+    autonomous_command(new PrintCommand("AutonomousCommand()")),
+    teleop_command(new PrintCommand("TeleopCommand()")) {
 
 }
 
 Robot::~Robot() { }
 
 void Robot::RobotInit() {
+  printf("RobotInit()");
   hardware_map.init();
   oi.init();
+
+  Robot::GetWatchdog().SetEnabled(false);
+}
+
+void Robot::DisabledInit() {
+  printf("DisabledInit()");
+}
+
+void Robot::DisabledPeriodic() {
+  printf("DisabledPeriodic()");
 }
 
 void Robot::AutonomousInit() {
-  autonomous_command.Start();
+  printf("AutonomousInit()");
+  autonomous_command->Start();
 }
 
 void Robot::AutonomousPeriodic() {
-  Scheduler::GetInstance().Run();
+  printf("AutonomousPeriodic()");
+  Scheduler::GetInstance()->Run();
 }
 
 void Robot::TeleopInit() {
-  autonomous_command.Cancel();
+  printf("TeleopInit()");
+  autonomous_command->Cancel();
 
-  teleop_command.Start();
+  teleop_command->Start();
 }
 
 void Robot::TeleopPeriodic() {
-  Scheduler::GetInstance().Run();
+  printf("TeleopPeriodic()");
+  Scheduler::GetInstance()->Run();
+  ds_lcd->UpdateLCD();
+}
+
+void Robot::TestInit() {
+  printf("TestInit()");
 }
 
 void Robot::TestPeriodic() {
-  LiveWindow::GetInstance().Run();
+  printf("TestPeriodic()");
+  LiveWindow::GetInstance()->Run();
 }
+
+START_ROBOT_CLASS(Robot);
