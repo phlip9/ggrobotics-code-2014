b20880598670a3073126004c0912bfd6e4ce39c7
Matthew Gee <geem2014@gmail.com>
2014-01-16 17:27:39 -0800

    Jaguars -> Talons

--- src/HardwareMap.cpp
+++ src/HardwareMap.cpp
@@ -3,17 +3,17 @@
 #include "HardwareMap.h"
 
 #include "SpeedController.h"
-#include "Jaguar.h"
+#include "Talon.h"
 #include "LiveWindow/LiveWindow.h"
 
 #include "Config.h"
 #include "Logging.h"
 
 HardwareMap::HardwareMap()
-  : front_left_motor(new Jaguar(CONFIG::MotorFrontLeftDrive())),
-    rear_left_motor(new Jaguar(CONFIG::MotorRearLeftDrive())),
-    front_right_motor(new Jaguar(CONFIG::MotorFrontRightDrive())),
-    rear_right_motor(new Jaguar(CONFIG::MotorRearRightDrive())),
+  : front_left_motor(new Talon(CONFIG::MotorFrontLeftDrive())),
+    rear_left_motor(new Talon(CONFIG::MotorRearLeftDrive())),
+    front_right_motor(new Talon(CONFIG::MotorFrontRightDrive())),
+    rear_right_motor(new Talon(CONFIG::MotorRearRightDrive())),
     gyro(1) {
 
 }
@@ -30,9 +30,9 @@ void HardwareMap::init() {
 
   LiveWindow *live_window = LiveWindow::GetInstance();
 
-  // Add Jaguar motors
-  live_window->AddActuator("Drive", "Front Left Motor", dynamic_cast<Jaguar*>(front_left_motor));
-  live_window->AddActuator("Drive", "Rear Left Motor", dynamic_cast<Jaguar*>(rear_left_motor));
-  live_window->AddActuator("Drive", "Front Right Motor", dynamic_cast<Jaguar*>(front_right_motor));
-  live_window->AddActuator("Drive", "Rear Right Motor", dynamic_cast<Jaguar*>(rear_right_motor));
+  // Add Talon motors
+  live_window->AddActuator("Drive", "Front Left Motor", dynamic_cast<Talon*>(front_left_motor));
+  live_window->AddActuator("Drive", "Rear Left Motor", dynamic_cast<Talon*>(rear_left_motor));
+  live_window->AddActuator("Drive", "Front Right Motor", dynamic_cast<Talon*>(front_right_motor));
+  live_window->AddActuator("Drive", "Rear Right Motor", dynamic_cast<Talon*>(rear_right_motor));
 }
