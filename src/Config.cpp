7178586b9d78380ac3376505ed414f3278fcf60a
Philip Hayes <philiphayes9@gmail.com>
2014-02-21 12:24:19 -0800

    Remove Gyro preferences and minor formatting changes.

--- src/Config.cpp
+++ src/Config.cpp
@@ -4,26 +4,14 @@
 
 #include "Preferences.h"
 
-float CONFIG::GyroScalingConstant() {
-  return Preferences::GetInstance()->GetFloat("GyroScalingConstant", -0.00875);
-}
-
-float CONFIG::GyroThreshold() {
-  return Preferences::GetInstance()->GetFloat("GyroThreshold", 1.0);
-}
-
-float CONFIG::GyroTimeout() {
-  return Preferences::GetInstance()->GetFloat("GyroTimeout", 0.25);
-}
-
 float CONFIG::ArmWheelPower() {
-  return Preferences::GetInstance()->GetFloat("ArmWheelPower", 0.5);
+  return Preferences::GetInstance()->GetFloat("ArmWheelPower", 0.50);
 }
 
 float CONFIG::FrontArmPowerUp() {
-  return Preferences::GetInstance()->GetFloat("FrontArmPowerUp", -0.50);
+  return Preferences::GetInstance()->GetFloat("FrontArmPowerUp", -0.80);
 }
 
 float CONFIG::FrontArmPowerDown() {
-  return Preferences::GetInstance()->GetFloat("FrontArmPowerDown", 0.30);
+  return Preferences::GetInstance()->GetFloat("FrontArmPowerDown", 0.50);
 }
--- src/Config.h
+++ src/Config.h
@@ -1,5 +1,8 @@
-// Robot config and hardware ids
-
+// Holds all the constants and smart dashboard preferences in a centralized
+// location. Should contain all the joystick ports, button numbers, motor
+// channels, solenoid inputs, etc... Functions that wrap the Preferences
+// object should go here too.  Preferences are constants that are displayed
+// and can be changed in the SmartDashboard on the driver station.
 
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
@@ -25,13 +28,13 @@ namespace CONFIG {
 
     const int compressor_switch = 10;
     const int compressor_relay = 1;
-  };
+  } // namespace HARDWARE
 
   namespace INPUT {
     // Drive Joystick port
     const int joystick_1 = 1;
 
-    // ## Buttons
+    // Joystick 1 Buttons
     const int gyro_toggle = 2;
 
     const int arm_wheel_forward = 1;
@@ -41,21 +44,9 @@ namespace CONFIG {
     const int front_arm_down = 5;
 
     const int shoot = 4;
-  };
+  } // namespace INPUT
 
   // ## Preferences (Can be edited on the SmartDashboard)
-  // The gyro scaling constant controls how strongly the gyro should correct.
-  // Make it too high, and the robot will start over-correcting, begin
-  // oscillating, and spin out of control.
-  // Make it too low, and the robot will correct too slowly.
-  float GyroScalingConstant();
-
-  // Don't adjust the robot unless the gyro passes the threshold.
-  float GyroThreshold();
-
-  // After we turn the joystick, don't enable the gyro until X seconds have
-  // passed.
-  float GyroTimeout();
 
   // How fast the wheel on the arm should spin.
   // (+) pushes the ball out
